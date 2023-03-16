426127044407a2238c5e54d66d449273c0ba3016
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-16 12:09:47 -0500

    Changed Comment in javascript

--- Develop/node.js
+++ Develop/node.js
@@ -1,8 +1,6 @@
 var APIKey = "48982f4209c09d080c28f41c9a165d5b";
 
-/* Need City to show following details, Temp, wind, humidty, and day status */
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+/* Need City to show following details, Temp, wind, humidty, and forcast */
 
 var city;
 [
