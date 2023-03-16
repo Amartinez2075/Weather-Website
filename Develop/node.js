2c00e34ed804e89d910b9235fb522011f3c0095d
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-16 12:06:03 -0500

    Tried Fixing the API need help

--- Develop/node.js
+++ Develop/node.js
@@ -1,9 +1,10 @@
-var APIKey = "6d226fd5f41a35166d199f21ed2431ef"
+var APIKey = "48982f4209c09d080c28f41c9a165d5b";
 
+/* Need City to show following details, Temp, wind, humidty, and day status */
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 
 var city;
 [
-    
-]
 
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6d226fd5f41a35166d199f21ed2431ef}
+]
