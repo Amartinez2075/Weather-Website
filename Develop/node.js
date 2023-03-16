3ae42c7352d7ee80896e770b2122f9603bf5a090
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-16 13:05:12 -0500

    Added another API

--- Develop/node.js
+++ Develop/node.js
@@ -1,8 +1,27 @@
-var APIKey = "48982f4209c09d080c28f41c9a165d5b";
+fetch(https://api.openweathermap.org/data/2.5/weather?lat=&lon=&appid=6d226fd5f41a35166d199f21ed2431ef)
+    {method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error}
+})
 
-/* Need City to show following details, Temp, wind, humidty, and forcast */
+fetch(http://api.openweathermap.org/geo/1.0/direct?q=city name,state code,country code&limit=limit&appid=6d226fd5f41a35166d199f21ed2431ef)
+    {method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error}
+})
 
-var city;
-[
+/* Well I need an api link to get me the city name Temp, wind, humidity, and forecast */
 
-]
+var cites = [
+{
+      City: "Atlanta",
+      City: "Denver",
+      City: "Seattle",
+      City: "San Francisco",
+      City: "Orland",
+      City: "New York",
+      City: "Chicago",
+      City: "Austin", 
+    },
+],
+/*1*/
