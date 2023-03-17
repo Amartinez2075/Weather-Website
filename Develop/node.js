767452fbaf65e45a29ba66c55093ef5bfcf2ba4c
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-17 16:42:18 -0500

    Variables

--- Develop/node.js
+++ Develop/node.js
@@ -1,44 +1,64 @@
-var lon = ""
-var lat = ""
-var city = ""
-var state code = ""
-var county code = ""
-var limit = ""
-var APIKey = ""
-var Weather = ""
+var lon = "";
+var lat = "";
+var city = "";
+var state = "";
+var county = "";
+var limit = "";
+var APIKey = "6d226fd5f41a35166d199f21ed2431ef";
+var Weather = "";
 
 // Get input from user (City/State)
 //A request to the 1st function to lat and lon
 //Once thats finished, do a request with the lat and long to the weather api aka Open Weather 
 //Now after that you want to render the data onscreen
 
-fetch('https://bulk.openweathermap.org/snapshot/hourly_zip_us.json.gz?appid={6d226fd5f41a35166d199f21ed2431ef',)
-[
-.function request(lat, lon)
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=&lon=&appid=6d226fd5f41a35166d199f21ed2431ef', {
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-}).then(function (res) {
-    return res.json()
-  }).then(function (data) {
-    console.log(data, "data")
+// Function to fetch weather data using latitude and longitude
+
+
+// Function to fetch weather data using latitude and longitude
+function fetchWeatherData(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    redirect: 'follow'
   })
-]
-[  Fetch('http://api.openweathermap.org/geo/1.0/direct?q=city name,state code,&limit=limit&appid={6d226fd5f41a35166d199f21ed2431ef}',{
-  method: 'GET', //GET is the default.
-  credentials: 'same-origin', // include, *same-origin, omit
-  redirect: 'follow', // manual, *follow, error
-  }).then(function (res) {
-    return res.json()
-  }).then(function (data) {
-    console.log(data, "data")
-  })]
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data, "data");
+    // Call function to render weather data on screen
+    renderWeatherData(data);
+  });
+}
 
+// Function to fetch location data using city and state name
+function fetchLocationData(city, state, limit) {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=${limit}&appid=${APIKey}`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    redirect: 'follow'
+  })
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data, "data");
+    // Get latitude and longitude from response data
+    const { lat, lon } = data[0];
+    // Call function to fetch weather data using latitude and longitude
+    fetchWeatherData(lat, lon);
+  });
+}
 
-// API finds, City Name, Lat and Lon, Weather, Wind
+// Function to render weather data on screen
+function renderWeatherData(data) {
+  // Code to render weather data on screen
+}
 
-// This fetch finds city name, State and state
+// Get input from user (City/State)
+// Call function to fetch location data using city and state name
+fetchLocationData(city, state, limit);
 
 // Get input from user (City/State)
 //A request to the 1st function to lat and lon
