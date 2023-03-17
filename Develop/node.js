59eaa1d940ca43804e0b4736e5300f98da74bbe6
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-16 22:07:20 -0500

    Waka Waka Added Instuctions

--- Develop/node.js
+++ Develop/node.js
@@ -1,4 +1,19 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=33.7488&lon=84.3877&appid=6d226fd5f41a35166d199f21ed2431ef', {
+var lon = ""
+var lat = ""
+var city = ""
+var state code = ""
+var county code = ""
+var limit = ""
+var APIKey = ""
+var Weather = ""
+
+// Get input from user (City/State)
+//A request to the 1st function to lat and lon
+//Once thats finished, do a request with the lat and long to the weather api aka Open Weather 
+//Now after that you want to render the data onscreen
+
+.function request(lat, lon)
+fetch('https://api.openweathermap.org/data/2.5/weather?lat={lon}8&lon={lon}&appid=6d226fd5f41a35166d199f21ed2431ef', {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -8,5 +23,18 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat=33.7488&lon=84.3877&a
     console.log(data, "data")
   })
 // API finds, City Name, Lat and Lon, Weather, Wind
-var city = "Buttons"
 
+Fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},&limit={limit}&appid={API key}'{
+method: 'GET', //GET is the default.
+credentials: 'same-origin', // include, *same-origin, omit
+redirect: 'follow', // manual, *follow, error
+}).then(function (res) {
+  return res.json()
+}).then(function (data) {
+  console.log(data, "data")
+})
+
+// Get input from user (City/State)
+//A request to the 1st function to lat and lon
+//Once thats finished, do a request with the lat and long to the weather api aka Open Weather 
+//Now after that you want to render the data onscreen
