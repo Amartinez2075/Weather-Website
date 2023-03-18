7b2353fd1c0cdfa7538fdc0e42cc8a8ed37eebe9
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-03-18 11:02:22 -0500

    Added javacode for errors

--- Develop/node.js
+++ Develop/node.js
@@ -1,15 +1,14 @@
-var lon = "20.00";
-var lat = "40.00";
+var lon = "33.7488° N";
+var lat = "84.3877° W";
 var city = "";
 var state = "";
 var country = "";
 var limit = "";
 var APIKey = "6d226fd5f41a35166d199f21ed2431ef";
-var Weather = "";
 
 // Function to fetch weather data using latitude and longitude
 function fetchWeatherData(lat, lon) {
-  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`, {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6d226fd5f41a35166d199f21ed2431ef`, {
     method: 'GET',
     credentials: 'same-origin',
     redirect: 'follow'
@@ -17,19 +16,20 @@ function fetchWeatherData(lat, lon) {
   .then(function (res) {
     return res.json();
   })
-  .then(function (data)) {
+  .then(function (data) {
     console.log(data, "data");
     // Call function to render weather data on screen
-  //  renderWeatherData(data);
-}}
-;
-
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+};
 
 // Function to fetch location data using city and state name
-// function fetchLocationData(city, state, country) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${APIKey}`, {
+function fetchLocationData(city, state, country) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=6d226fd5f41a35166d199f21ed2431ef`, {
     method: 'GET',
-   credentials: 'same-origin',
+    credentials: 'same-origin',
     redirect: 'follow'
   })
   .then(function (res) {
@@ -41,19 +41,15 @@ function fetchWeatherData(lat, lon) {
     const { lat, lon } = data.coord;
     // Call function to fetch weather data using latitude and longitude
     fetchWeatherData(lat, lon);
+  })
+  .catch(function (err) {
+    console.error(err);
   });
-
-// Function to render weather data on screen
-function renderWeatherData(data) {
-  // Code to render weather data on screen
-}
-
-// Call function to fetch location data using city, state, and country name
-fetchLocationData(city, state, country);
+};
 
 // Function to fetch weather data for bulk files using a zip file
-function fetchBulkFiles(zipFile, APIKey) {
-  fetch(`https://bulk.openweathermap.org/archive/${zipFile}?appid=${APIKey}`, {
+function fetchBulkFiles(zipFile) {
+  fetch(`https://bulk.openweathermap.org/archive/weather_14.json.gz?appid=6d226fd5f41a35166d199f21ed2431ef`, {
     method: 'GET',
     credentials: 'same-origin',
     redirect: 'follow'
@@ -67,8 +63,12 @@ function fetchBulkFiles(zipFile, APIKey) {
     const { lat, lon } = data.city.coord;
     // Call function to fetch weather data using latitude and longitude
     fetchWeatherData(lat, lon);
+  })
+  .catch(function (err) {
+    console.error(err);
   });
-}
+};
+
 
 
 // Get input from user (City/State) (COMPLETE)
